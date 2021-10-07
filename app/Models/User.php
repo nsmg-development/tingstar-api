0393c6eb9d9c6915a324684505f136431878c993
Orgebattle <orgebattlesaga@gmail.com>
2021-10-07 20:20:14 +0900

    fix: indexing

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,6 +14,7 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    protected $connection = 'curator9-common';
     /**
      * The attributes that are mass assignable.
      *
